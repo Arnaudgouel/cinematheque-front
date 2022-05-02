d708daf4a8cd5da77d889f1d789ce21f5ec69de2
Arnaud Gouel <gouel.arnaud@gmail.com>
2022-05-02 16:45:48 +0200

    struturing the app

--- src/components/Film/FilmList.jsx
+++ src/components/Film/FilmList.jsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Film } from "./";
+import { Film } from "./Film";
 
 const FilmList = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  let params = useParams()
-
   useEffect(() => {
-    fetch("https://127.0.0.1:8000/films-acteurs/recherche?search=" + params.search)
+    fetch("https://127.0.0.1:8000/films")
       .then(res => res.json())
       .then(
         (res) => {
@@ -43,7 +40,7 @@ const FilmList = () => {
                 <span className="placeholder col-6"></span>
                 <span className="placeholder col-8"></span>
               </p>
-              <p className="btn btn-primary disabled placeholder col-6"></p>
+              <p className="btn btn-success disabled placeholder col-6"></p>
             </div>
           </div>
         </div>
@@ -52,7 +49,7 @@ const FilmList = () => {
   }
 
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="row rows-col-md-3 rows-col-sm-2 rows-col-1 g-3">
         {items.map(item => <Film key={item.id_film} item={item}/>)}
       </div>
